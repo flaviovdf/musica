367e02201bc032e6674de9a2bbfab63704112821
Flavio Figueiredo <flaviovdf@gmail.com>
2021-12-10 19:00:18 -0300

    Update files.py

--- μsica/files.py
+++ μsica/files.py
@@ -11,19 +11,15 @@ import warnings
 def download(url, outfpath, verify=True):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        filesize = int(requests.head(url).headers["Content-Length"])
         bar = tqdm.tqdm(unit="B",
                         unit_scale=True,
                         unit_divisor=1024,
-                        total=filesize,
                         file=sys.stdout,
                         desc=outfpath)
 
-        with requests.get(url, stream=True, verify=verify) as r:
-            # r.raise_for_status()
-            with open(outfpath, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8192): 
-                    f.write(chunk)
+        with open(outfpath, 'wb') as f:
+            with requests.get(url, stream=True, verify=verify) as r:
+                for chunk in r.iter_content(chunk_size=4096): 
                     datasize = f.write(chunk)
                     bar.update(datasize)
 
