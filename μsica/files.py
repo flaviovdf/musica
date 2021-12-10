3282dbd79e7e5f88f1163f529caa639be20c7a19
Flavio Figueiredo <flaviovdf@gmail.com>
2021-12-10 18:45:23 -0300

    Update files.py

--- μsica/files.py
+++ μsica/files.py
@@ -3,16 +3,28 @@
 
 import requests
 import shutil
+import sys
 import tqdm
 
 
 def download(url, outfpath, verify=True):
-    response = requests.get(url, stream=True, verify=verify)
-    
-    with open(outfpath, "wb") as handle:
-        for data in tqdm.tqdm(response.iter_content()):
-            handle.write(data)
-
+    filesize = int(requests.head(url).headers["Content-Length"])
+    bar = tqdm.tqdm(unit="B", # unit string to be displayed.
+                    unit_scale=True, # let tqdm to determine the scale in kilo, mega..etc.
+                    unit_divisor=1024, # is used when unit_scale is true
+                    total=filesize, # the total iteration.
+                    file=sys.stdout, # default goes to stderr, this is the display on console.
+                    desc=outfpath) # prefix to be displayed on progress bar.
+        
+    with requests.get(url, stream=True, verify=verify) as r:
+        r.raise_for_status()
+        with open(outfpath, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192): 
+                f.write(chunk)
+                datasize = f.write(chunk)
+                bar.update(datasize)
+        
+                
             
 def extract(zipfpath, where):
     shutil.unpack_archive(zipfpath, where)
