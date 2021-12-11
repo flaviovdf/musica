1f2e554afb32667e4d3bb09b686bc3734f7a832e
Flavio Figueiredo <flavio@dcc.ufmg.br>
2021-12-11 11:53:16 -0300

    added notebooks

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ if __name__ == "__main__":
     if sys.version_info[:2] < (3, 6):
         print('Requires Python version 3.6 or later')
         sys.exit(-1)
-          
+
     setup(name='musica_dcc_ufmg',
           packages=['μsica'],
+          install_requires=[line.strip() for line in open('requirements.txt')],
           version='0.01',
           description='μsica utilities for the MIR course at UFMG',
           author='Flavio Figueiredo')
